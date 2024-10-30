febce7687966e438070a389b7035a68eadcf2bf5
lambypy <danbud@hotmail.co.uk>
2024-10-30 15:27:22 +0000

    Added more information for SavingsAccount

--- src/main/java/com/lbg/banking/SavingsAccount.java
+++ src/main/java/com/lbg/banking/SavingsAccount.java
@@ -1,6 +1,15 @@
 package com.lbg.banking;
 
 public class SavingsAccount {
+    private long balance;
+    private long minimumBalance;
+    private String accountHolderName;
+
+    public SavingsAccount(long balance, long minimumBalance, String accountHolderName){
+        this.balance = balance;
+        this.minimumBalance = minimumBalance;
+        this.accountHolderName = accountHolderName;
+    }
 
     public SavingsAccount(){};
 
